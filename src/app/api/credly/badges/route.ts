edc9af227ff7361f9274a3b019fe9c406a8a44e8
Ellyo Freitas <43623016+ellyofreitas@users.noreply.github.com>
2025-03-27 20:35:46 -0300

    fix badge api route

--- src/app/api/credly/badges/route.ts
+++ src/app/api/credly/badges/route.ts
@@ -1,5 +1,7 @@
 const { CREDLY_USERNAME: credlyUsername } = process.env
 
+export const dynamic = 'force-static'
+
 export async function GET() {
   const response = await fetch(
     `https://www.credly.com/users/${credlyUsername}/badges?sort=rank`,
