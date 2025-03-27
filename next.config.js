d36c01b321a5b932293d5565d6e7ea127cc66fd7
Ellyo Freitas <43623016+ellyofreitas@users.noreply.github.com>
2025-03-27 20:10:38 -0300

    fix images

--- next.config.js
+++ next.config.js
@@ -1,11 +1,18 @@
+const nextEnvUrl = process.env.NEXT_PUBLIC_SITE_URL
+
+if (!nextEnvUrl) {
+  throw new Error('NEXT_PUBLIC_SITE_URL is required')
+}
+
+const nextUrl = new URL(nextEnvUrl)
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   devIndicators: false,
   images: {
     remotePatterns: [
-      {
-        hostname: 'images.credly.com',
-      },
+      { hostname: 'images.credly.com' },
+      { hostname: nextUrl.hostname },
     ],
   },
 }
